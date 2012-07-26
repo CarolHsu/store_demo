67bd50239cd4de5eef084e9369aa569a553c5691
Carol Hsu <sheseee@gmail.com>
2012-07-26 22:02:13 +0800

    promoting the views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(
+  :title => "Programming Ruby 1.9",
+  :description => "<p>Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox.</p>",
+  :image_url => "/images/ruby.jpg",
+  :price => 49.50
+)
